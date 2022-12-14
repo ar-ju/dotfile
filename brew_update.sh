6a992615b2b735f6c16797b8954e57ac765d33d5
kageyama <kageyama@v6.netsci.info.hiroshima-cu.ac.jp>
2022-12-14 20:35:08 +0900

    コメントを追加

--- brew_update.sh
+++ brew_update.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
 # 毎週決まった時間にbrew updateを行ってくれるスクリプト
+# を作りたい
+# 決まった時間は今の所、「毎週月曜日午前9時」にしている
 
 now_weekd=`date +%u`
+now_time=`date +%H`
 
 if [ "$now_weekd" -eq 1 ];then
-    now_time=`date +%H`
     if [ "$now_time" -ge 9 ] && [  ];then
         
     fi
